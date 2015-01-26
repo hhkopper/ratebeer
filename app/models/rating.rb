f9254d08e5f18b89615d09cc12191a7579c559e4
wadror <wadror@wadror-VirtualBox.(none)>
2015-01-26 15:49:56 +0200

    ratings maara ja keskiarvo

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,8 +1,9 @@
 class Rating < ActiveRecord::Base
 	belongs_to :beer
+	belongs_to :user
 
-		def to_s
-			b = Beer.find_by id:self.beer_id
-			"#{b.name} #{self.score}"
-		end
+	def to_s
+		b = Beer.find_by id:self.beer_id
+		"#{b.name} #{self.score}"
+	end
 end
