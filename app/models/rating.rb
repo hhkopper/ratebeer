3c1f664c9ae124505ffc94bd4d47bc63b48be9f4
wadror <wadror@wadror-VirtualBox.(none)>
2015-01-26 23:03:00 +0200

    listataan antaja

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,4 +6,9 @@ class Rating < ActiveRecord::Base
 		b = Beer.find_by id:self.beer_id
 		"#{b.name} #{self.score}"
 	end
+
+	def usersName
+		user = User.find_by id:self.user_id
+		"#{user.username}"
+	end
 end
