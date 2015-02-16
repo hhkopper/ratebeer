28eef49b9b3a264fa860a738938f3da45efe748b
wadror <wadror@wadror-VirtualBox.(none)>
2015-02-16 11:41:14 +0200

    testi

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,7 +55,7 @@ end
 
 def sign_in(credentials)
     visit signin_path
-    fill_in('username', with:credentials[:username])
-    fill_in('password', with:credentials[:password])
+    fill_in('username', :with => credentials[:username])
+    fill_in('password', :with => credentials[:password])
     click_button('Log in')
 end
