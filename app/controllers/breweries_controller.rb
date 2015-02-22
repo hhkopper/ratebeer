afde0c3ed0baaa5172214e49a5446499ef4d7e19
wadror <wadror@wadror-VirtualBox.(none)>
2015-02-22 16:26:15 +0200

    jarjestysta voi vaihtaa

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -7,6 +7,18 @@ class BreweriesController < ApplicationController
   def index
     @active_breweries = Brewery.active
     @retired_breweries = Brewery.retired
+
+    order = params[:order] || 'name'
+
+    if session[:sorted_by].nil?
+	session[:sorted_by] = "asc"
+    end
+
+    if session[:sorted_by] == "asc"
+    	sort_normal(order)
+    else
+	sort_reverse(order)
+    end
   end
 
   # GET /breweries/1
@@ -81,5 +93,29 @@ class BreweriesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def brewery_params
       params.require(:brewery).permit(:name, :year, :active)
+    end
+
+    def sort_reverse(order)
+	session[:sorted_by] = "asc"
+	@active_breweries = case order
+		when 'name' then @active_breweries.sort_by{ |b| b.name }.reverse
+		when 'year' then @active_breweries.sort_by{ |b| b.year }.reverse	
+    	end
+	@retired_breweries = case order
+		when 'name' then @retired_breweries.sort_by{ |b| b.name }.reverse
+		when 'year' then @retired_breweries.sort_by{ |b| b.year }.reverse	
+    	end
+    end
+
+    def sort_normal(order)
+	@active_breweries = case order
+		when 'name' then @active_breweries.sort_by{ |b| b.name }
+		when 'year' then @active_breweries.sort_by{ |b| b.year }	
+    	end
+	@retired_breweries = case order
+		when 'name' then @retired_breweries.sort_by{ |b| b.name }
+		when 'year' then @retired_breweries.sort_by{ |b| b.year }	
+    	end
+	session[:sorted_by] = "desc"
     end
 end
